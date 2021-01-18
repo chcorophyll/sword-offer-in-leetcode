74e983559614a23b7dbe3fca9ca39528ac4e3259
chcorophyll <chcorophyll@gmail.com>
2021-01-18 09:04:23 +0800

    offer_67 done

---
+++ offer_67.py
@@ -0,0 +1,18 @@
+class Solution:
+    def strToInt(self, str: str) -> int:
+        s = str.strip()
+        if not s:
+            return 0
+        result, index, sign = 0, 1, 1
+        int_max, int_min, boundry_num = 2 ** 31 - 1, -2 ** 31, 2 ** 31 // 10
+        if s[0] == "-":
+            sign = -1
+        elif s[0] != "+":
+            index = 0
+        for k in s[index:]:
+            if not "0" <= k <= "9":
+                break
+            if result > boundry_num or (result == boundry_num and k > "7"):
+                return int_max if sign == 1 else int_min
+            result = result * 10 + ord(k) - ord("0")
+        return result * sign      
